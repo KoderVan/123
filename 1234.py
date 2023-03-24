e5d1a089c3bdd3f56d880c21a2c588ae405aa67e
KoderVan <vancrash00@gmail.com>
2023-03-24 12:30:39 +0500

    Init commit

---
+++ 1234.py
@@ -0,0 +1,17 @@
+line = input()
+l = len(line)
+num = ''
+letter = ''
+for char in line:
+    if char.isalpha():
+        if num != '':
+            num = int(num)
+            print(letter*num)
+        letter = char
+        num = ''
+    if char.isdigit():
+        num = num + char
+print(letter*int(num))
+
+
+
